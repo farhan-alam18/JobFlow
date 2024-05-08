e20767f98919d872dc348873373ab9e58b22237a
farhan-alam18 <farhanalam1113@gmail.com>
2024-05-08 19:34:41 +0530

    Final Changes

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,12 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Metadata } from "next";
 import AdminNavbar from "./AdminNavbar";
-import { auth } from "@clerk/nextjs/server";
 
 export const metadata: Metadata = {
   title: "Admin",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    auth().protect()
   return (
     <ClerkProvider>
       <AdminNavbar />
