05cf8697ea9a1f52b82afdde0bedebea6771861d
farhan-alam18 <farhanalam1113@gmail.com>
2024-05-08 19:47:29 +0530

    Final Changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
-import { clerkMiddleware} from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware()
+export default clerkMiddleware();
 
 export const config = {
   // The following matcher runs middleware on all routes
   // except static assets.
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/(admin)(.*)"],
 };
